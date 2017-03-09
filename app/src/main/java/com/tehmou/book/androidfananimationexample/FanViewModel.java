ae7bc60b63599becf050d7787f6bc4b74f0a3b72
tehmou <timo.tuominen@gmail.com>
2017-03-09 14:19:51 +0000

    Add FanViewModel and opening logic

---
+++ app/src/main/java/com/tehmou/book/androidfananimationexample/FanViewModel.java
@@ -0,0 +1,36 @@
+package com.tehmou.book.androidfananimationexample;
+
+import android.util.Log;
+
+import java.util.List;
+
+import rx.Observable;
+import rx.subjects.BehaviorSubject;
+
+public class FanViewModel {
+    private static final String TAG = FanViewModel.class.getSimpleName();
+
+    private final BehaviorSubject<List<FanItem>> fanItems;
+    private final BehaviorSubject<Boolean> isOpen = BehaviorSubject.create(false);
+    private final BehaviorSubject<Float> openRatio = BehaviorSubject.create();
+
+    public FanViewModel(Observable<Void> clickObservable,
+                        List<FanItem> fanItems) {
+        this.fanItems = BehaviorSubject.create(fanItems);
+        clickObservable
+                .doOnNext(click -> Log.d(TAG, "click"))
+                .subscribe(click -> isOpen.onNext(!isOpen.getValue()));
+         AnimateToOperator.animate(
+                 isOpen.map(value -> value ? 1f : 0f), 200)
+                 .doOnNext(value -> Log.d(TAG, "value: " + value))
+                 .subscribe(openRatio::onNext);
+    }
+
+    public Observable<Float> getOpenRatio() {
+        return openRatio;
+    }
+
+    public Observable<List<FanItem>> getFanItems() {
+        return fanItems;
+    }
+}
